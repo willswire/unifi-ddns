770faf421732bbe8cbc0fb81bc93f932a8ef9e1c
William Walker <w_walker@icloud.com>
2024-12-28 22:32:15 -0500

    wip: working out tests

--- src/index.ts
+++ src/index.ts
@@ -1,213 +1,114 @@
-// Exceptions
-class BadRequestException extends Error {
-	status: number;
-	statusText: string;
+import { ClientOptions, Cloudflare } from 'cloudflare';
+import { AAAARecord, ARecord } from 'cloudflare/src/resources/dns/records.js';
+type AddressableRecord = AAAARecord | ARecord;
 
-	constructor(reason: string) {
-		super(reason);
-		this.status = 400;
-		this.statusText = 'Bad Request';
+class HttpError extends Error {
+	constructor(
+		public statusCode: number,
+		message: string,
+	) {
+		super(message);
+		this.name = 'HttpError';
 	}
 }
 
-class CloudflareApiException extends Error {
-	status: number;
-	statusText: string;
-
-	constructor(reason: string) {
-		super(reason);
-		this.status = 500;
-		this.statusText = 'Internal Server Error';
-	}
-}
-
-// Cloudflare API Handler
-interface CloudflareOptions {
-	token: string;
-}
-
-interface DNSRecord {
-	id: string;
-	zone_id: string;
-	type: string;
-	content: string;
-}
-
-interface Zone {
-	id: string;
-	name: string;
-}
-
-class Cloudflare {
-	private cloudflare_url: string;
-	private token: string;
-
-	constructor(options: CloudflareOptions) {
-		this.cloudflare_url = 'https://api.cloudflare.com/client/v4';
-		this.token = options.token;
-	}
-
-	async findZone(name: string): Promise<Zone> {
-		const response = await this._fetchWithToken(`zones?name=${name}`);
-		const body = (await response.json()) as any;
-
-		if (!body.success || body.result.length === 0) {
-			throw new CloudflareApiException(`Failed to find zone '${name}'`);
-		}
-		return body.result[0];
-	}
-
-	async findRecord(zone: Zone, name: string, isIPV4: boolean = true): Promise<DNSRecord> {
-		const rrType = isIPV4 ? 'A' : 'AAAA';
-		const response = await this._fetchWithToken(`zones/${zone.id}/dns_records?name=${name}`);
-		const body = (await response.json()) as any;
-
-		if (!body.success || body.result.length === 0) {
-			throw new CloudflareApiException(`Failed to find DNS record '${name}'`);
-		}
-		return body.result.filter((rr: DNSRecord) => rr.type === rrType)[0];
-	}
-
-	async updateRecord(record: DNSRecord, value: string): Promise<DNSRecord> {
-		record.content = value;
-		const response = await this._fetchWithToken(`zones/${record.zone_id}/dns_records/${record.id}`, {
-			method: 'PUT',
-			body: JSON.stringify(record),
-		});
-		const body = (await response.json()) as any;
-
-		if (!body.success) {
-			throw new CloudflareApiException('Failed to update DNS record');
-		}
-		return body.result[0];
-	}
-
-	private async _fetchWithToken(endpoint: string, options: RequestInit = {}): Promise<Response> {
-		const url = `${this.cloudflare_url}/${endpoint}`;
-		options.headers = {
-			...options.headers,
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${this.token}`,
-		};
-		return fetch(url, options);
-	}
-}
-
-// HTTPS Enforcement
-function requireHttps(request: Request): void {
-	const { protocol } = new URL(request.url);
-	const forwardedProtocol = request.headers.get('x-forwarded-proto');
-
-	if (protocol !== 'https:' || forwardedProtocol !== 'https') {
-		throw new BadRequestException('Please use a HTTPS connection.');
-	}
-}
-
-// Basic Auth Parsing
-interface AuthCredentials {
-	username?: string;
-	password?: string;
-}
-
-function parseBasicAuth(request: Request): AuthCredentials {
+function constructClientOptions(request: Request): ClientOptions {
 	const authorization = request.headers.get('Authorization');
-	if (!authorization) return {};
+	if (!authorization) {
+		throw new HttpError(401, 'API token missing.');
+	}
 
 	const [, data] = authorization.split(' ');
 	const decoded = atob(data);
 	const index = decoded.indexOf(':');
 
 	if (index === -1 || /[\0-\x1F\x7F]/.test(decoded)) {
-		throw new BadRequestException('Invalid authorization value.');
+		throw new HttpError(401, 'Invalid API key or token.');
 	}
 
 	return {
-		username: decoded.substring(0, index),
-		password: decoded.substring(index + 1),
+		apiEmail: decoded.substring(0, index),
+		apiToken: decoded.substring(index + 1),
 	};
 }
 
-// Handle Request
-async function handleRequest(request: Request): Promise<Response> {
-	requireHttps(request);
-	const { pathname } = new URL(request.url);
-
-	if (pathname === '/favicon.ico' || pathname === '/robots.txt') {
-		return new Response(null, { status: 204 });
-	}
-
-	if (!pathname.endsWith('/update')) {
-		return new Response('Not Found.', { status: 404 });
-	}
-
-	if (!request.headers.has('Authorization') && !request.url.includes('token=')) {
-		return new Response('Not Found.', { status: 404 });
-	}
-
-	const { username, password } = parseBasicAuth(request);
+function constructDNSRecord(request: Request): AddressableRecord {
 	const url = new URL(request.url);
 	const params = url.searchParams;
+	const ip = params.get('ip');
+	const hostname = params.get('hostname');
 
-	const token = password || params.get('token');
-
-	const hostnameParam = params.get('hostname') || params.get('host') || params.get('domains');
-	const hostnames = hostnameParam?.split(',');
-
-	const ipsParam = params.get('ips') || params.get('ip') || params.get('myip') || request.headers.get('Cf-Connecting-Ip');
-	const ips = ipsParam?.split(',');
-
-	if (!hostnames || hostnames.length === 0 || !ips || ips.length === 0) {
-		throw new BadRequestException('You must specify both hostname(s) and IP address(es)');
+	if (ip === null || ip === undefined) {
+		throw new HttpError(422, 'The "ip" parameter is required and cannot be empty.');
 	}
 
-	for (const ip of ips) {
-		await informAPI(hostnames, ip.trim(), username || '', token || '');
+	if (hostname === null || hostname === undefined) {
+		throw new HttpError(422, 'The "hostname" parameter is required and cannot be empty.');
 	}
 
-	return new Response('good', {
-		status: 200,
-		headers: {
-			'Content-Type': 'text/plain;charset=UTF-8',
-			'Cache-Control': 'no-store',
-		},
-	});
+	return {
+		content: ip,
+		name: hostname,
+		type: ip.includes('.') ? 'A' : 'AAAA',
+		ttl: 1,
+	};
 }
 
-// Inform Cloudflare API
-async function informAPI(hostnames: string[], ip: string, name: string, token: string): Promise<void> {
-	const cloudflare = new Cloudflare({ token });
+async function update(clientOptions: ClientOptions, newRecord: AddressableRecord): Promise<Response> {
+	const cloudflare = new Cloudflare(clientOptions);
 
-	const isIPV4 = ip.includes('.'); // Simple IPv4 check
-	const zones = new Map<string, Zone>();
+	const tokenStatus = (await cloudflare.user.tokens.verify()).status;
 
-	for (const hostname of hostnames) {
-		const domainName = name && hostname.endsWith(name) ? name : hostname.replace(/.*?([^.]+\.[^.]+)$/, '$1');
-
-		if (!zones.has(domainName)) {
-			zones.set(domainName, await cloudflare.findZone(domainName));
-		}
-
-		const zone = zones.get(domainName)!;
-		const record = await cloudflare.findRecord(zone, hostname, isIPV4);
-		await cloudflare.updateRecord(record, ip);
+	if (tokenStatus !== 'active') {
+		throw new HttpError(401, 'This API Token is ' + tokenStatus);
 	}
+
+	const zones = (await cloudflare.zones.list()).result;
+	const zone = zones.pop();
+
+	if (zones.length > 1) {
+		throw new HttpError(400, 'More than one zone was found! You must supply an API Token scoped to a single zone.');
+	} else if (zone === undefined) {
+		throw new HttpError(400, 'No zones found! You must supply an API Token scoped to a single zone.');
+	}
+
+	const records = (
+		await cloudflare.dns.records.list({
+			zone_id: zone.id,
+			name: newRecord.name,
+			type: newRecord.type,
+		})
+	).result;
+	const oldRecord = records.pop();
+
+	if (records.length > 1) {
+		throw new HttpError(400, 'More than one matching record found!');
+	} else if (oldRecord === undefined || oldRecord.id === undefined) {
+		throw new HttpError(400, 'No record found! You must first manually create the record.');
+	}
+
+	await cloudflare.dns.records.update(oldRecord.id, {
+		content: newRecord.content,
+		zone_id: zone.id,
+		name: newRecord.name,
+		type: newRecord.type,
+	});
+
+	return new Response(null, { status: 200 });
 }
 
 export default {
 	async fetch(request): Promise<Response> {
-		return handleRequest(request).catch((err: any) => {
-			console.error(err.constructor.name, err);
-			const message = err.message || err.stack || 'Unknown Error';
-
-			return new Response(message, {
-				status: err.status || 500,
-				statusText: err.statusText || 'Internal Server Error',
-				headers: {
-					'Content-Type': 'text/plain;charset=UTF-8',
-					'Cache-Control': 'no-store',
-					'Content-Length': String(message.length),
-				},
-			});
-		});
+		try {
+			const clientOptions = constructClientOptions(request);
+			const record = constructDNSRecord(request);
+			return update(clientOptions, record);
+		} catch (error) {
+			if (error instanceof HttpError) {
+				return new Response(error.message, { status: error.statusCode });
+			} else {
+				return new Response('Internal Server Error', { status: 500 });
+			}
+		}
 	},
 } satisfies ExportedHandler<Env>;
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,25 +1,10 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
+import { SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
-import worker from '../src/index';
 
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
-
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
-
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+describe('UniFi DDNS Worker', () => {
+	it('responds with 401 unauthorized', async () => {
+		const response = await SELF.fetch('http://example.com/update?ip=192.0.2.1&hostname=home.example.com');
+		expect(response.status).toMatchInlineSnapshot(`401`);
 	});
 });
